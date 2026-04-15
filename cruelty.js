ee74fe387d5725c596a1deb1da2da03644f912a4
SlasherFree <ultrastalker27@gmail.com>
2026-04-16 01:17:12 +0200

    Update cruelty.js

--- cruelty.js
+++ cruelty.js
@@ -52,7 +52,7 @@ const scratchedState = [
     false, false, false, false,
     false, false, true, false,
     false, false, false, false,
-    false, false, false, false
+    false, true, false, false
 ];
 
 // Inicializar el grid de rasca y gana
